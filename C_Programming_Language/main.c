52e9884038f13b449aab93a187b5afbdce5f2f19
Carlos Rios <rioscarlosd@gmail.com>
2012-10-07 10:47:18 -0500

    Initial Commit

---
+++ C_Programming_Language/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C_Programming_Language
+//
+//  Created by Carlos Rios on 10/7/12.
+//  Copyright (c) 2012 Ideaware Co. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
