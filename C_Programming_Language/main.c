273a5d06ef1043e3b87c9b8be716dd1971535108
Carlos Rios <rioscarlosd@gmail.com>
2012-10-07 10:49:18 -0500

    Exercise 1-8

--- C_Programming_Language/main.c
+++ C_Programming_Language/main.c
@@ -8,11 +8,21 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(){
+    int c, bs = 0, t = 0, nl = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ')
+            ++bs;
+        else if (c == '\t')
+            ++t;
+        else if (c == '\n')
+            ++nl;
+        //putchar(c);
+        printf("%c __ bs: %d t: %d nl: %d\n", c, bs, t, nl);
+    }
+    
 }
-
