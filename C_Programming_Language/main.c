cade13d6a6f75e44de236cc12c66aed68adb11a6
Carlos Rios <rioscarlosd@gmail.com>
2013-03-01 00:18:34 -0500

    Exercise 2-4

--- C_Programming_Language/main.c
+++ C_Programming_Language/main.c
@@ -7,53 +7,33 @@
 //
 
 #include <stdio.h>
-int lower(int c);
-int isLetter(int c);
-int isDigit(int c);
-int isResDig(int c);
-int htoi(char h[]);
+void squeeze(char s[], char c[]);
 
 int main(){
     
-    int test = 0xff24;
-    char hnum[] = "ff24";
+    char name[] = "mi nombre es carlos rios";
+    char cdel[] = "ai";
     
-    printf("%d -- %d", test, htoi(hnum));
+    squeeze(name, cdel);
+    
+    printf("%s", name);
     
     return 0;
 }
-int htoi(char h[]){
-    int n = 0, i, start = 0;
+
+void squeeze(char s[], char c[]){
+    int i, j, k, fnd;
     
-    if (h[0] == '0' && lower(h[1]) == 'x')
-        start = 2;
-    
-    for (i = start; isDigit(h[i]) || isLetter(h[i]); i++) {
-        int vd = isDigit(h[i]) ? h[i] - '0' : lower(h[i]) - 'a' + 10;
-        n = 16 * n + vd;
+    for (i = k = 0; s[i] != '\0'; i++){
+        for (j = fnd = 0; c[j] != '\0' && !fnd; j++){
+            if (s[i] == c[j]){
+                fnd++;
+            }
+        }
+        if (!fnd)
+            s[k++] = s[i];
     }
+    s[k] = '\0';
     
-    return n;
-}
-
-int isResDig(int c){
-    return (lower(c) == 'x');
-}
-
-int isDigit(int c){
-    return (c >= '0' && c <= '9');
-}
-
-int isLetter(int c){
-    
-    int lc = lower(c);
-    return (lc >= 'a' && lc<= 'f');
-}
-
-int lower(int c){
-    if (c >= 'A' && c <= 'Z')
-        return c + 'a' - 'A';
-    else
-        return c;
 }
 
