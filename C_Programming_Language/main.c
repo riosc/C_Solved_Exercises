994a894c4223f03491e473241a8433c7f6aa5ad1
Carlos Rios <rioscarlosd@gmail.com>
2013-03-09 21:10:25 -0500

    Example 3-3

--- C_Programming_Language/main.c
+++ C_Programming_Language/main.c
@@ -7,16 +7,32 @@
 //
 
 #include <stdio.h>
-int lower(int c);
+int binsearch(int x, int v[], int n);
 
 int main()
 {
-    char h = 'A';
-    putchar(lower(h));
+    int v[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    printf("pos: %d", binsearch(10, v, 10));
     return 0;
 }
 
-int lower(int c)
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch(int x, int v[], int n)
 {
-    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+    int low, hight, mid;
+    low             = 0;
+    hight           = n - 1;
+    
+    while (low <= hight) {
+        mid = (low + hight)/2;
+
+        if (x < v[mid])
+            hight   = mid + 1;
+        else if (x > v[mid])
+            low     = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
 }
